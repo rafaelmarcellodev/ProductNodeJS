9a2d8eea0b912d78cfe37a0f4c55c53c32de5361
Rafael Marcello <rafael.marcello.dev@gmail.com>
2024-04-27 16:54:19 -0300

    feature: Adiciona validacao na rota com middleware

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const ProductController = require('../controllers/productController');
+const validateProduct = require('../middlewares/productValidator');
 
 const router = express.Router();
 
-router.post('/products', ProductController.createProduct);
+router.post('/products', validateProduct, ProductController.createProduct);
 router.get('/products', ProductController.getProducts);
 router.get('/products/:id', ProductController.getProductById);
 router.get('/products/:name', ProductController.getProductByName);
-router.put('/products/:id', ProductController.updateProduct);
+router.put('/products/:id', validateProduct, ProductController.updateProduct);
 router.delete('/products/:id', ProductController.deleteProduct);
 
 module.exports = router;
